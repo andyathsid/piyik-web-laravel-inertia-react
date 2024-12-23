46e721c82c5bb41f0bccfbd3b0a3d17cf7a02106
andyathsid <andakaraas@gmail.com>
2024-12-23 18:59:31 +0700

    Change logout request method to get

--- routes/auth.php
+++ routes/auth.php
@@ -55,6 +55,6 @@ Route::middleware('auth.firebase')->group(function () {
 
     // Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+    Route::get('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
 });
